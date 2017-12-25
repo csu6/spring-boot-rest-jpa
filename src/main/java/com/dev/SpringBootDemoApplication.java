1b311a8bd3fa3a750bfc5f538a473f2cf8ab44ee
csu6 <csuwannapak@gmail.com>
2017-12-25 18:44:36 +0100

    Ajout d'un bean et appel via le fichier config bean xml
    resources/app-config-ou-beans.xml

--- src/main/java/com/dev/SpringBootDemoApplication.java
+++ src/main/java/com/dev/SpringBootDemoApplication.java
@@ -2,11 +2,23 @@ package com.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.dev.model.HelloWorld;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
+		HelloWorld h = new HelloWorld();
+		h.setMessage("Le monde !!");
+		h.getMessage();
+		System.out.printf("\n-------------------------------------------------------------------------\n");
+		ApplicationContext context = new ClassPathXmlApplicationContext("app-config-ou-beans.xml");
+		HelloWorld h2 = (HelloWorld) context.getBean("helloWorld");
+		System.out.printf("\n-------------------------------helloWorld VIA BEAN-----------------------------------------\n");
+		h2.getMessage();
+
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 }
